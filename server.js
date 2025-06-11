ff0682a1c90e35a6c5b9ad38f6c82babf2e317e6
khamthit <khamtid@gmail.com>
2025-06-11 17:06:33 +0700

    update 11062025 evenint
    update 11062025 evening

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ app.post("/api/addsocketTicketChatnote", upload.single("file"), (req, res) => {
   if (req.file) {
     fileUrl = `http://10.0.100.31:4478/${req.file.filename}`;
   }
-
+  const readdata = "false";
   const id = Math.random().toString(36).substr(2, 9);
   const newData = {
     id,
@@ -183,6 +183,7 @@ app.post("/api/addsocketTicketChatnote", upload.single("file"), (req, res) => {
     fileUrl,
     To,
     Groupchat,
+    readdata,
     timestamp: new Date().toISOString()
   };
 
@@ -191,6 +192,135 @@ app.post("/api/addsocketTicketChatnote", upload.single("file"), (req, res) => {
   io.emit("addTicketChatNote", socketDataChatNote);
 
   res.status(201).json({ message: "Data added successfully", data: newData });
+});
+
+app.post("/api/addsocketTicketChatnoteAdmin", upload.single("file"), (req, res) => {
+  const { username, Message, To, Groupchat } = req.body;
+
+  if (!username || !Message || !To) {
+    return res.status(400).json({ error: "Username, Message, and To are required" });
+  }
+
+  let fileUrl = null;
+  if (req.file) {
+    fileUrl = `http://10.0.100.31:4478/${req.file.filename}`;
+  }
+  const readdata = "false";
+  const typechat = "admin";
+  const id = Math.random().toString(36).substr(2, 9);
+  const newData = {
+    id,
+    username,
+    Message,
+    fileUrl,
+    To,
+    Groupchat,
+    readdata,
+    typechat,
+    timestamp: new Date().toISOString()
+  };
+
+  socketDataChatNote.push(newData);
+  saveToHistory(newData);
+  io.emit("addTicketChatNote", socketDataChatNote);
+
+  res.status(201).json({ message: "Data added successfully", data: newData });
+});
+
+app.post("/api/addsocketTicketChatnoteAirline", upload.single("file"), (req, res) => {
+  const { username, Message, To, Groupchat } = req.body;
+
+  if (!username || !Message || !To) {
+    return res.status(400).json({ error: "Username, Message, and To are required" });
+  }
+
+  let fileUrl = null;
+  if (req.file) {
+    fileUrl = `http://10.0.100.31:4478/${req.file.filename}`;
+  }
+  const readdata = "false";
+  const typechat = "airline";
+  const id = Math.random().toString(36).substr(2, 9);
+  const newData = {
+    id,
+    username,
+    Message,
+    fileUrl,
+    To,
+    Groupchat,
+    readdata,
+    typechat,
+    timestamp: new Date().toISOString()
+  };
+
+  socketDataChatNote.push(newData);
+  saveToHistory(newData);
+  io.emit("addTicketChatNote", socketDataChatNote);
+
+  res.status(201).json({ message: "Data added successfully", data: newData });
+});
+app.post("/api/markAsReadbyAirline/:To/:From", (req, res) => {
+  const { To, From } = req.params;
+
+  // Find the message by ID
+  // const message = socketDataChatNote.find(user => user.To === To && user.username === From);
+  const updatedMessages = socketDataChatNote.filter(user => user.To === To && user.username === From && user.typechat === "airline");
+
+   if (!updatedMessages) {
+    return res.status(404).json({ error: `Message with id "${To}" not found` });
+  }
+   updatedMessages.forEach(message => {
+    message.readdata = true; // Set 'readdata' to true for each matching message
+  });
+  res.json({ message: "Message marked as read", updatedMessages });
+});
+
+app.post("/api/markAsReadbyAdmin/:From/:To", (req, res) => {
+  const { From, To } = req.params;
+
+  // Find the message by ID
+  // const message = socketDataChatNote.find(user => user.To === To && user.username === From);
+  const updatedMessages = socketDataChatNote.filter(user => user.username === From && user.To === To && user.typechat === "admin");
+
+   if (!updatedMessages) {
+    return res.status(404).json({ error: `Message with id "${To}" not found` });
+  }
+   updatedMessages.forEach(message => {
+    message.readdata = true; // Set 'readdata' to true for each matching message
+  });
+  res.json({ message: "Message marked as read", updatedMessages });
+});
+
+app.get("/api/countUnreadMessagesAirline/:To/:From", (req, res) => {
+  const { To, From } = req.params;
+
+  // Find all messages that match the "To", "From", and "readdata = false"
+  const unreadMessages = socketDataChatNote.filter(
+    message => message.To === To && message.username === From && JSON.parse(message.readdata) === false && message.typechat === "airline"
+  );
+
+  console.log("to:", To);
+  console.log("from:", From);
+  console.log("unreadMessages:", unreadMessages.length);
+
+  // Return the count of unread messages
+  res.json({ unreadCount: unreadMessages.length });
+});
+
+app.get("/api/countUnreadMessagesAdmin/:From/:To", (req, res) => {
+  const { From, To } = req.params;
+
+  // Find all messages that match the "To", "From", and "readdata = false"
+  const unreadMessages = socketDataChatNote.filter(
+    message => message.username === From && message.To === To && JSON.parse(message.readdata) === false && message.typechat === "admin"
+  );
+
+  console.log("to:", To);
+  console.log("from:", From);
+  console.log("unreadMessages:", unreadMessages.length);
+
+  // Return the count of unread messages
+  res.json({ unreadCount: unreadMessages.length });
 });
 
 app.get("/api/socketDataChatNote/:id", (req, res) => {
